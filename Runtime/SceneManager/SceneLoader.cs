107a1f3285f44aa36fcbb4ddf734abbcbc4ba3f3
seungbin0619 <seungbin0619@naver.com>
2026-03-25 12:45:44 +0900

    feat: SceneLoader 구현

---
+++ Runtime/SceneManager/SceneLoader.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UniSceneManager = UnityEngine.SceneManagement.SceneManager;
+
+#if ONION_ADDRESSABLES
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+#endif
+
+namespace Onion.SceneManagement {
+    internal abstract class SceneLoader {
+        protected SceneReference reference { get; }
+
+        private Scene _scene;
+        public Scene scene {
+            get => _scene;
+            protected set {
+                if (value == _scene) return;
+
+                _scene = value;
+                _scene.Register(loader: this);
+            }
+        }
+
+        protected SceneLoader(SceneReference reference) {
+            this.reference = reference;
+        }
+
+        public abstract Awaitable<bool> LoadAsync();
+        public abstract Awaitable UnloadAsync();
+    }
+
+    internal class BuiltInSceneLoader : SceneLoader {
+        private const float loadThreshold = 0.9f;
+        public BuiltInSceneLoader(SceneReference reference) : base(reference) { }
+
+        public override async Awaitable<bool> LoadAsync() {
+            UniSceneManager.sceneLoaded += OnSceneLoaded;
+
+            var operation = UniSceneManager.LoadSceneAsync(reference.path, LoadSceneMode.Additive);
+            operation.allowSceneActivation = false;
+
+            while (operation.progress < loadThreshold) {
+                await Awaitable.NextFrameAsync();
+            }
+
+            operation.allowSceneActivation = true;
+
+            await operation;
+            await Awaitable.NextFrameAsync(); // wait for scene to be fully loaded
+            UniSceneManager.sceneLoaded -= OnSceneLoaded;
+
+            return scene.isLoaded;
+        }
+
+        public override async Awaitable UnloadAsync() {
+            await UniSceneManager.UnloadSceneAsync(scene);
+
+            scene.Unregister();
+        }
+
+        private void OnSceneLoaded(Scene loadedScene, LoadSceneMode mode) {
+            if (loadedScene.HasLoader()) return;
+            if (loadedScene.path == reference.path) {
+                scene = loadedScene;
+            }
+        }
+    }
+
+    internal class AddressableSceneLoader : SceneLoader {
+        public AddressableSceneLoader(SceneReference reference) : base(reference) { }
+
+#if ONION_ADDRESSABLES
+        private AsyncOperationHandle<SceneInstance> _handle;
+#endif
+
+        public override async Awaitable<bool> LoadAsync() {
+#if ONION_ADDRESSABLES
+            _handle = Addressables.LoadSceneAsync(reference.path, LoadSceneMode.Additive);
+
+            var instance = await _handle.Task;
+            scene = instance.Scene;
+
+            return _handle.Status == AsyncOperationStatus.Succeeded;
+#else
+#pragma warning disable 1998
+            throw new System.NotSupportedException("Addressable scene loading is not supported.");
+#pragma warning restore 1998
+#endif
+        }
+
+        public override async Awaitable UnloadAsync() {
+#if ONION_ADDRESSABLES
+            if (!_handle.IsValid()) return;
+            
+            await Addressables.UnloadSceneAsync(_handle, true).Task;
+            _handle = default;
+
+            scene.Unregister();
+#else
+#pragma warning disable 1998
+            throw new System.NotSupportedException("Addressable scene unloading is not supported.");
+#pragma warning restore 1998
+#endif
+        }
+    }
+}
---
+++ Runtime/SceneManager/SceneLoaderExtensions.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace Onion.SceneManagement {
+    internal static class SceneLoaderExtensions {
+        private static readonly Dictionary<Scene, SceneLoader> _loaders = new();
+
+        public static void Register(this Scene scene, SceneLoader loader) {
+            if (!scene.IsValid()) {
+                return;
+            }
+
+            _loaders[scene] = loader;
+        }
+
+        public static void Unregister(this Scene scene) {
+            if (scene.IsValid()) {
+                _loaders.Remove(scene);
+            }
+        }
+
+        public static bool HasLoader(this Scene scene) {
+            return _loaders.ContainsKey(scene);
+        }
+
+        public static bool TryGetLoader(this Scene scene, out SceneLoader loader) {
+            return _loaders.TryGetValue(scene, out loader);
+        }
+
+        public static SceneLoader GetLoader(this Scene scene) {
+            if (TryGetLoader(scene, out var loader)) {
+                return loader;
+            }
+
+            return null;
+        }
+    }
+}
