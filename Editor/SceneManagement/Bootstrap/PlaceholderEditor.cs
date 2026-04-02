be04427ce766208d8bd6907145609e1ac461b05e
seungbin0619 <seungbin0619@naver.com>
2026-04-03 01:22:38 +0900

    test: Placeholder 테스트

--- Editor/SceneManagement/Bootstrap/PlaceholderEditor.cs
+++ Editor/SceneManagement/Bootstrap/PlaceholderEditor.cs
@@ -6,12 +6,15 @@ namespace Onion.SceneManagement.Editor {
     [CanEditMultipleObjects]
     internal sealed class PlaceholderEditor : UnityEditor.Editor {
         private const string bootstrapSceneWarning = "This is the bootstrap scene.\nPlaceholder is not allowed here.";
+        private const string bootstrapSceneInfo = "Bootstrap scene is not enabled in the settings.";
         private const string placeholderInfo = "This object will be destroyed at runtime.";
 
         public override void OnInspectorGUI() {
             // base.OnInspectorGUI();
 
-            if (SceneManagementSettings.useBootstrapScene) {
+            if (!SceneManagementSettings.useBootstrapScene) {
+                EditorGUILayout.HelpBox(bootstrapSceneInfo, MessageType.Info);
+
                 return;
             }
 
