0c2b6581c81eaf64769b379286285696419e7787
seungbin0619 <seungbin0619@naver.com>
2026-04-05 12:19:18 +0900

    chore: 불필요한 Bootstrapper 기능 제거

--- Runtime/SceneManager/Bootstrap/Bootstrapper.cs
+++ Runtime/SceneManager/Bootstrap/Bootstrapper.cs
@@ -8,57 +8,7 @@
 using UnityEngine;
 
 namespace Onion.SceneManagement {
-    public static class Bootstrapper {
-        public static bool isReady { get; internal set; }
-
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-        private static void Initialize() {
-            
-        }
+    internal static class Bootstrapper {
+        internal static bool isReady { get; set; }
     }
-
-//     [AddComponentMenu("Bootstrapper")]
-//     [DisallowMultipleComponent]
-//     public class Bootstrapper : MonoBehaviour {
-//         public static bool isReady { get; private set; }
-
-//         [SerializeField]
-//         private GameObject[] preloadPrefabs;
-
-//         [Space(8)]
-//         [SerializeField]
-//         private UnityEvent completed;
-
-//         protected virtual void Awake() {
-//             foreach (var obj in preloadPrefabs) {
-//                 if (obj == null) {
-//                     continue;
-//                 }
-
-//                 Instantiate(obj);
-//             }
-
-//             isReady = true;
-//             completed?.Invoke();
-//         }
-
-//         protected virtual void OnValidate() {
-// #if UNITY_EDITOR
-//             if (preloadPrefabs == null) {
-//                 return;
-//             }
-
-//             for (int i = 0; i < preloadPrefabs.Length; i++) {
-//                 if (preloadPrefabs[i] == null) {
-//                     continue;
-//                 }
-
-//                 if (PrefabUtility.GetPrefabAssetType(preloadPrefabs[i]) == PrefabAssetType.NotAPrefab) {
-//                     Debug.LogError($"Element at index {i} in _preloadPrefabs is not a prefab.", this);
-//                     preloadPrefabs[i] = null;
-//                 }
-//             }
-// #endif
-//         }
-//     }
 }
